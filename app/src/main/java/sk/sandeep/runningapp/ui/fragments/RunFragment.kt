4dc2ad3d98c14266163bc478e14c3fd4326ec19c
Sandeep <sandeepsk15892>
2022-12-14 10:24:59 +0530

    add permissions settings

--- app/src/main/java/sk/sandeep/runningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/sk/sandeep/runningapp/ui/fragments/RunFragment.kt
@@ -1,5 +1,7 @@
 package sk.sandeep.runningapp.ui.fragments
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,13 +10,16 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 import sk.sandeep.runningapp.R
 import sk.sandeep.runningapp.databinding.FragmentRunBinding
-import sk.sandeep.runningapp.databinding.FragmentSetupBinding
+import sk.sandeep.runningapp.util.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import sk.sandeep.runningapp.util.TrackingUtility
 import sk.sandeep.runningapp.view_model.MainViewModel
 
 @AndroidEntryPoint
-class RunFragment : Fragment() {
+class RunFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -25,11 +30,59 @@ class RunFragment : Fragment() {
     ): View {
         val binding = FragmentRunBinding.inflate(inflater, container, false)
         context ?: return binding.root
-
+        requestPermission()
         binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
 
         return binding.root
     }
+
+
+    private fun requestPermission() {
+        if (TrackingUtility.hasLocationPermissions(requireContext())) {
+            return
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You Need To Accept Location Permissions To  Use This App.",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            )
+        }else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You Need To Accept Location Permissions To  Use This App.",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+            )
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        if(EasyPermissions.somePermissionPermanentlyDenied(this,perms)){
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermission()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {}
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // EasyPermissions handles the request result.
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
 }
--- app/src/main/java/sk/sandeep/runningapp/util/Constants.kt
+++ app/src/main/java/sk/sandeep/runningapp/util/Constants.kt
@@ -2,4 +2,5 @@ package sk.sandeep.runningapp.util
 
 object Constants{
     const val RUNNING_DATABASE_NAME="running_db"
+    const val REQUEST_CODE_LOCATION_PERMISSION=0
 }
